3b15a69761ac8068f594252e32cdf38359b2b8c7
Christian Bepler <bepler135@gmail.com>
2024-11-08 11:00:16 -0500

    remove accidental input

--- cpu/reg-file/DFFtri.v
+++ cpu/reg-file/DFFtri.v
@@ -1,5 +1,5 @@
 module DFFtri(out, d, clk, clr, in_enable, out_enable);
-    input d, clk, clr, pr, in_enable, out_enable;
+    input d, clk, clr, in_enable, out_enable;
     output out;
     wire q;
 
