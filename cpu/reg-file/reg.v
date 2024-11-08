1f6f0ce15239257a9a6c0917817c24f25805921f
Christian Bepler <bepler135@gmail.com>
2024-11-08 12:19:40 -0500

    fix typo

--- cpu/reg-file/reg.v
+++ cpu/reg-file/reg.v
@@ -7,7 +7,7 @@ module register(out, d, clk, clr, read_enable, write_enable);
 
     generate
         for (i = 0; i < 32; i = i + 1) begin : dff_loop
-            dffe_ref a_dff(.q(out), .d(d), .clk(clk), .en(write_enable), .clr(clr));
+            dffe_ref a_dff(.q(out[i]), .d(d[i]), .clk(clk), .en(write_enable), .clr(clr));
         end
     endgenerate
 
