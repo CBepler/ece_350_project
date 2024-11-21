06d6275f7af805d60d61f4d586fce3d4bd942609
Christian Bepler <bepler135@gmail.com>
2024-11-21 18:45:09 -0500

    update badic movement and compile

--- cpu/cpu/processor/project_files/assembly_files/basic_box_movement.s
+++ cpu/cpu/processor/project_files/assembly_files/basic_box_movement.s
@@ -5,16 +5,25 @@
 # 300 - 399 is vga x
 # 400 - 499 is vga y
 
+#Storage Idea: move where in memory the head is each time. The memory shifts not the data
+#add delay to slow down the snake
+#hold current direction in memory and then updare it when needed and always update position based on direction
+
 sw $r0, 100($r0)  #100 is local x of box
 sw $r0, 200($r0)  #200 is local y of box
 
 sw $r0, 1($r0) #zero out game_done
+sw $r0, 2($r0) #zero out previous button state
+
 
 start_loop:
 lw $r1, 0($r0)  #load in buttons value
 lw $r2, 100($r0) #load local x
 lw $r3, 101($r0) #load local y
+lw $r5, 2($r0) #previous button state
 addi $r4, $r0, 1 #button checker
+
+bne $r5, $r0 button_done
 
 bne $r1, $r4, not_up
 addi $r3, $r3, -1
@@ -40,6 +49,8 @@ j button_done
 not_left:
 
 button_done:
+
+sw $r1 2($r0)
 
 #local store back
 sw $r2, 100($r0)
