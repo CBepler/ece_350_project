d1795603c264290e5686451d71fa8c3a54908d8f
raquiqui <117326145+raquiqui@users.noreply.github.com>
2024-11-21 20:21:56 -0500

    Add files via upload

--- cpu/cpu/processor/RAM.v
+++ cpu/cpu/processor/RAM.v
@@ -26,3 +26,28 @@ module RAM #( parameter DATA_WIDTH = 32, ADDRESS_WIDTH = 12, DEPTH = 4096) (
         end
     end
 endmodule
+
+module RAM_v #( parameter DATA_WIDTH = 8, ADDRESS_WIDTH = 8, DEPTH = 256, MEMFILE = "") (
+    input wire                     clk,
+    input wire                     wEn,
+    input wire [ADDRESS_WIDTH-1:0] addr,
+    input wire [DATA_WIDTH-1:0]    dataIn,
+    output reg [DATA_WIDTH-1:0]    dataOut);
+    
+    reg[DATA_WIDTH-1:0] MemoryArray[0:DEPTH-1];
+    
+    initial begin
+        if(MEMFILE > 0) begin
+            $readmemh(MEMFILE, MemoryArray);
+        end
+    end
+    
+    always @(posedge clk) begin
+        if(wEn) begin
+            MemoryArray[addr] <= dataIn;
+        end else begin
+            dataOut <= MemoryArray[addr];
+        end
+    end
+endmodule
+
