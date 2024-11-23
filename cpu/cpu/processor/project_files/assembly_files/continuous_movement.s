24812741f324f3577436a62a6874dbbdcacb67e0
Christian Bepler <bepler135@gmail.com>
2024-11-23 12:30:20 -0500

    fix end game logic and add stall loop

--- cpu/cpu/processor/project_files/assembly_files/continuous_movement.s
+++ cpu/cpu/processor/project_files/assembly_files/continuous_movement.s
@@ -12,7 +12,7 @@
 sw $r0, 100($r0)  #100 is local x of box
 sw $r0, 200($r0)  #200 is local y of box
 
-sw $r0, 1($r0) #zero out game_done
+#sw $r0, 1($r0) #zero out game_done
 sw $r0, 2($r0) #zero out previous button state
 
 
@@ -21,6 +21,19 @@ sw $r1 3($r0) #start direction as right
 
 
 start_loop:
+
+#stall start_loop
+addi $r3, $r0, 600
+addi $r4, $r0, 0
+addi $r1, $r0, 30000
+outer_stall_loop
+addi $r2, $r0, 0
+inner_stall_loop:
+addi $r2, 1
+blt $r2, $r1, inner_stall_loop
+addi $r4, $r0, 1
+blt $r4, $r3 outer_stall_loop
+
 lw $r1, 0($r0)  #load in buttons value
 lw $r2, 100($r0) #load local x
 lw $r3, 200($r0) #load local y
@@ -30,28 +43,7 @@ addi $r4, $r0, 1 #button checker
 
 bne $r5, $r0 button_done
 
-bne $r1, $r4, not_up
-addi $r6, $r0, 1
-j button_done
-not_up:
-
-addi $r4, $r4, 1
-bne $r1, $r4, not_right
-addi $r6, $r0, 2
-j button_done
-not_right:
-
-addi $r4, $r4, 1
-bne $r1, $r4, not_down
-addi $r6, $r0, 3
-j button_done
-not_down:
-
-addi $r4, $r4, 1
-bne $r1, $r4, not_left
-addi $r6, $r0, 4
-j button_done
-not_left:
+bne $r1, $r0, new_direction
 
 button_done:
 
@@ -85,6 +77,19 @@ direction_done:
 
 sw $r1 2($r0) #button as new previous button
 
+
+addi $r4, $r0, 9 #check if out of bounds
+blt $r2, $r0, done #branch if 0 > x
+blt $r4, $r2, done #branch if x > 10
+blt $r3, $r0, done #branch if 0 > y
+blt $r4, $r3, done #branch if y > 10
+
+#addi $r4, $r0, 1
+#sw $r4, 1($r0)
+#j game_done
+
+update:
+
 #local store back
 sw $r2, 100($r0)
 sw $r3, 200($r0)
@@ -93,21 +98,16 @@ sw $r3, 200($r0)
 sw $r2, 300($r0)
 sw $r3, 400($r0)
 
-
-addi $r4, $r0, 10 #check if out of bounds
-blt $r0, $r2, not_done #branch if 0 < x  ie  x > 0
-blt $r2, $r4, not_done #branch if x < 10
-blt $r0, $r3, not_done #branch if 0 < x  ie  x > 0
-blt $r3, $r4, not_done #branch if y < 10
-
-addi $r4, $r0, 1
-sw $r4, 1($r0)
-j game_done
-
-not_done:
-
 j start_loop
 
+#game_done:
 
-game_done:
+done:
+addi $r2, $r0, 0
+addi $r3, $r0, 0
+j update
 
+
+new_direction:
+addi $r6, $r1, 0
+j button_done
