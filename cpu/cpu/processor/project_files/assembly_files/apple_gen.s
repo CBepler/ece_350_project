70acbbc62acbc8b663c1f8426c846161be4ebc29
Christian Bepler <bepler135@gmail.com>
2024-11-25 16:49:28 -0500

    move generation after snake update and compile

--- cpu/cpu/processor/project_files/assembly_files/apple_gen.s
+++ cpu/cpu/processor/project_files/assembly_files/apple_gen.s
@@ -55,59 +55,6 @@ lw $r8, 6($r0) #food need
 addi $r4, $r0, 1
 
 
-#food_generation_section
-bne $r8, $r4, food_done
-#sw $r0, 6($r0) #no food needed next time   (commented out so it contionuosly generates for testing)
-lw $r9, 7($r0) #load in random x value
-lw $r10, 8($r0) #load in random y value
-
-restart_check_loop:
-addi $r11, $r0, 0 # Counter for checking snake parts
-check_snake_loop:
-blt $r7, $r11, food_position_valid # If we've checked all snake parts, position is valid
-
-# Load snake part coordinates
-addi $r12, $r11, 100 # Get address of x coordinate
-lw $r13, 0($r12) # Load x coordinate
-addi $r12, $r11, 200 # Get address of y coordinate
-lw $r14, 0($r12) # Load y coordinate
-
-# Check if food position matches this snake part
-bne $r9, $r13, position_different # If x coordinates don't match, check next part
-bne $r10, $r14, position_different # If y coordinates don't match, check next part
-
-# Position matches snake part, increment x coordinate
-addi $r9, $r9, 1
-addi $r15, $r0, 9
-blt $r15, $r9, wrap_x # If x > 9, wrap to 0
-j restart_check_loop # Restart check with new position
-
-wrap_x:
-addi $r9, $r0, 0 # Set x back to 0
-addi $r10, $r10, 1 # Increment y
-addi $r15, $r0, 9
-blt $r15, $r10, wrap_y # If y > 9, wrap to 0
-j restart_check_loop # Restart check with new position
-
-wrap_y:
-addi $r10, $r0, 0 # Set y back to 0
-j restart_check_loop # Restart check with new position
-
-position_different:
-addi $r11, $r11, 1 # Increment counter
-j check_snake_loop
-
-food_position_valid:
-# Store final food position
-sw $r9, 7($r0) # Store food x position
-sw $r10, 8($r0) # Store food y position
-# Update VGA display for food
-sw $r9, 9($r0) # Store food x position in VGA
-sw $r10, 10($r0) # Store food y position in VGA
-
-food_done:
-
-
 bne $r5, $r0 button_done
 
 bne $r1, $r0, new_direction
@@ -181,6 +128,59 @@ sw $r3, 200($r0)
 #VGA store back for head
 sw $r2, 300($r0)
 sw $r3, 400($r0)
+
+
+#food_generation_section
+bne $r8, $r4, food_done
+#sw $r0, 6($r0) #no food needed next time   (commented out so it contionuosly generates for testing)
+lw $r9, 7($r0) #load in random x value
+lw $r10, 8($r0) #load in random y value
+
+restart_check_loop:
+addi $r11, $r0, 0 # Counter for checking snake parts
+check_snake_loop:
+blt $r7, $r11, food_position_valid # If we've checked all snake parts, position is valid
+
+# Load snake part coordinates
+addi $r12, $r11, 100 # Get address of x coordinate
+lw $r13, 0($r12) # Load x coordinate
+addi $r12, $r11, 200 # Get address of y coordinate
+lw $r14, 0($r12) # Load y coordinate
+
+# Check if food position matches this snake part
+bne $r9, $r13, position_different # If x coordinates don't match, check next part
+bne $r10, $r14, position_different # If y coordinates don't match, check next part
+
+# Position matches snake part, increment x coordinate
+addi $r9, $r9, 1
+addi $r15, $r0, 9
+blt $r15, $r9, wrap_x # If x > 9, wrap to 0
+j restart_check_loop # Restart check with new position
+
+wrap_x:
+addi $r9, $r0, 0 # Set x back to 0
+addi $r10, $r10, 1 # Increment y
+addi $r15, $r0, 9
+blt $r15, $r10, wrap_y # If y > 9, wrap to 0
+j restart_check_loop # Restart check with new position
+
+wrap_y:
+addi $r10, $r0, 0 # Set y back to 0
+j restart_check_loop # Restart check with new position
+
+position_different:
+addi $r11, $r11, 1 # Increment counter
+j check_snake_loop
+
+food_position_valid:
+# Store final food position
+sw $r9, 7($r0) # Store food x position
+sw $r10, 8($r0) # Store food y position
+# Update VGA display for food
+sw $r9, 9($r0) # Store food x position in VGA
+sw $r10, 10($r0) # Store food y position in VGA
+
+food_done:
 
 
 j start_loop
